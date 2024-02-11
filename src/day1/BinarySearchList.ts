61b98970354b271d5cb3910a453c58c981562262
Martin Grellet <martin.alejandro.grellet@gmail.com>
2024-02-11 10:16:23 -0300

    02 - Binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,16 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let lo = 0;
+    let hi = haystack.length;
+    do {
+        let m = Math.floor(lo + ((hi - lo) / 2)); // Gets the middle of array
+        let v = haystack[m];
+        if (v === needle) {
+            return true;
+        } else if (v > needle) {// needle is on the  first half of array
+            hi = m;
+        } else { // needle is on the second half of arr ay
+            lo = m + 1;
+        }
+    } while (lo < hi);
+    return false;
 }
