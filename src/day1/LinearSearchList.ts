da8c31d88b947c533ac4a32d0cd8dcd170a25bac
Martin Grellet <martin.alejandro.grellet@gmail.com>
2024-02-11 08:58:00 -0300

    01 - Linear search

---
+++ src/day1/LinearSearchList.ts
@@ -0,0 +1,9 @@
+// @ts-ignore
+export default function linear_search(haystack: number[], needle: number): boolean {
+    for(const element of haystack) {
+        if (element === needle) {
+            return true;
+        }
+    }
+    return false;
+}
