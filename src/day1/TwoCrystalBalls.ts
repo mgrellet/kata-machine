54f9f5db4b535b2fe09ff42f12f2f8fda0166b7b
Martin Grellet <martin.alejandro.grellet@gmail.com>
2024-02-11 17:55:28 -0300

    03 - Binary search: two_crystal_balls example

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,23 @@
+// Given two crystal balls that will break if dropped from high enough
+// distance, determine the exact spot in which it will break in the most
+// optimized way.
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+    const jmpAmount = Math.floor(Math.sqrt(breaks.length))
+    let i = jmpAmount;
+
+    for (; i < breaks.length; i = i + jmpAmount) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    i = i - jmpAmount;
+
+    for (let j = 0; j < jmpAmount && i < breaks.length; i++, j++) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+    return -1;
 }
